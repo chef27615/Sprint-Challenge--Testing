4812d7b97be36ea3d0d123c31dbb844261f2d748
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-24 13:18:56 -0400

    post endpoint tested with positive results

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const gamesList = require('../gamesList/gamesModel');
-
+const knex = require('knex');
 
 const server = express();
 
@@ -15,4 +15,8 @@ server.get('/games', async (req, res) => {
     res.status(200).json(game)
 })
 
+server.post('/games', async (req, res) => {
+    const games = await gamesList.getAll();
+    res.status(201).json(games)
+})
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const server = require('./server');
+const db = require('../data/dbConfig');
+const Games = require('../gamesList/gamesModel');
 
 describe('server', () => {
     it('should testing in the right environment', () => {
@@ -7,6 +9,11 @@ describe('server', () => {
     })
 
     describe('endpoints', () => {
+        beforeEach(async () => {
+            await db('games').truncate();
+        })
+
+
         describe('GET /', () => {
             it('should return 200', async () => {
                 const res = await request(server).get('/');
@@ -14,10 +21,42 @@ describe('server', () => {
             })
         })
 
-        describe('/games', () => {
+        describe('GET /games', () => {
             it('should return 200', async () => {
                 const res = await request(server).get('/games');
                 expect(res.status).toBe(200)
+            })
+
+            it('should return an empty array of games', async () => {
+                const games = await db('games');
+                expect(games).toHaveLength(0)
+            })
+            
+            it('should return array of games', async () => {
+                await Games.insert({title: 'heroKey', genre: 'RPG', releaseYear: '1994'});
+                const games = await db('games');
+                expect(games).toHaveLength(1)
+            })
+        })
+
+        describe('POST /game', () => {
+            beforeEach(async () => {
+                await db('games').truncate();
+            })
+
+            it('should insert games', async () => {
+                await Games.insert({ title: 'bandit luke', genre: 'Action', releaseYear: '2010'});
+                const games = await db('games');
+                expect(games).toHaveLength(1)
+            })
+
+            it('should create game object', async () => {
+                await Games.insert({ title: 'luke story', genre: 'Action', releaseYear: '2011'});
+                const res = await request(server).get('/games');
+                const data = res.body;
+                expect(res.status).toBe(200)
+                expect(data).toHaveLength(1)
+                expect(data[0].title).toBe('luke story')
             })
         })
     })
