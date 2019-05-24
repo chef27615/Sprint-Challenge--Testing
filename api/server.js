c42d1f02ff524b33095d25f2583cfee0beae5379
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-24 11:30:16 -0400

    dependencies added, database test file successfully created

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+    res.status(200).json({ message: "good to go"});
+})
+
+server.get('/game', async (req, res) => {
+    const game = await gamesList.getAll()
+    res.status(200).json(game)
+})
+
+module.exports = server;
